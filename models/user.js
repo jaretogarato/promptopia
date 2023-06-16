bc5288fab2e8c48c54732a30950bde9099a9496d
Jared Gallardo <jared@jareds-mbp.lan>
2023-06-15 21:32:05 -0600

    fix authorization bug; add Google profile image icons

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 import { Schema, model, models } from 'mongoose'
-import { stringify } from 'postcss'
 
 const UserSchema = new Schema({
 	email: {
@@ -9,10 +8,11 @@ const UserSchema = new Schema({
 	},
 	username: {
 		type: String,
+		unique: [true, 'Username already exists!'],
 		required: [true, 'Username is required!'],
 		match: [
-			/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-aA-A0-9._]+(?<![_.])$/,
-			'Username invalid; it should contain 8-20 alphanumeric characters and be unique.',
+			/^[a-zA-Z0-9]{8,20}$/,
+			'Username invalid, it should contain 8-20 alphanumeric characters and be unique!',
 		],
 	},
 	image: {
